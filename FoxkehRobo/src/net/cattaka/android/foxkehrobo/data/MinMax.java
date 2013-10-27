00dc1b419cedc301bfe396e187e61fe8ebeb0544
cattaka <cattaka@mail.cattaka.net>
2013-10-28 03:18:46 +0900

    Refine AI actions.

---
+++ FoxkehRobo/src/net/cattaka/android/foxkehrobo/data/MinMax.java
@@ -0,0 +1,21 @@
+
+package net.cattaka.android.foxkehrobo.data;
+
+public class MinMax {
+    public final int yawMin;
+
+    public final int yawMax;
+
+    public final int pitchMin;
+
+    public final int pitchMax;
+
+    public MinMax(int yawMin, int yawmax, int pitchMin, int pitchMax) {
+        super();
+        this.yawMin = yawMin;
+        this.yawMax = yawmax;
+        this.pitchMin = pitchMin;
+        this.pitchMax = pitchMax;
+    }
+
+}
